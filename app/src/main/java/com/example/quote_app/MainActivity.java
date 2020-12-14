f468ea24570201343b6faa893c8be00a1e0e7749
SaraOrlian <orlian.sara@gmail.com>
2020-12-14 17:43:23 -0500

    added fab button with next text

--- app/src/main/java/com/example/quote_app/MainActivity.java
+++ app/src/main/java/com/example/quote_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quote_app;
 
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,14 +21,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+       setupFAB();
+    }
+
+    private void setupFAB() {
+        ExtendedFloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                handleFABclick();
             }
         });
+    }
+
+    private void handleFABclick() {
+        //to be developed...
     }
 @Override
     public boolean onCreateOptionsMenu(Menu menu) {
